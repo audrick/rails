49a06d50208bfeec424c7a969c7dede18411ebdf
Audric Kim <audrickim@gmail.com>
2012-12-06 09:32:37 -0500

    dec 6 2012

---
+++ quizzes/hydrogen/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydrogen::Application.config.secret_token = '95e346f65875a1ea1049775730c07ccf5b3b45a22e730332eef79163c193545ee2276fd0d38168c6a14a9fb2ffc80c8795d2eb86778c3da79e5a40f0cef43088'
